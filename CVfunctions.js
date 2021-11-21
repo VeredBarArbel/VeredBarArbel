c36a46875959550c452a3a0933d17e849957edc1
VeredBarArbel <veredarbel@gmail.com>
2021-11-21 17:23:30 +0200

    ex 5 - animation & sticky position

---
+++ CVfunctions.js
@@ -0,0 +1,8 @@
+//print my name to the console
+console.log("Welcome to my Online CV page");
+console.log("My name is Vered-Bar Arbel");
+
+//change #imgID's src to newSrc. both should by string type
+function changeImgSrc(imgID, newSrc){
+    document.getElementById(imgID).src = newSrc;
+}
